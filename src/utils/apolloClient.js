cbdb7ab745cbb351f9e479d1bfb250af4778c968
Solo Hsu <53265779+SoloHsu-coder@users.noreply.github.com>
2022-12-13 22:57:22 +0630

    fix url

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -4,7 +4,7 @@ import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
+  uri: "http://0.0.0.0:7131/admin",
 });
 const client = new ApolloClient({
   cache,
