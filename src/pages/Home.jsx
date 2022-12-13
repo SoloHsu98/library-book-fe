b35f41c1f41a56b2ad98802255830d8af3062d44
Solo Hsu <53265779+SoloHsu-coder@users.noreply.github.com>
2022-12-13 21:14:49 +0630

    fix name error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BestSellers from "../component/BestSellers";
 import Footer from "../component/Footer";
-import Herosection from "../component/herosection";
+import Herosection from "../component/Herosection";
 import Navbar from "../component/Navbar";
 import NewItem from "../component/NewItem";
 
